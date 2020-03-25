5c79f896f8c0f00f3a0e15cd1720c1a0e8023702
moe9195 <moerahmeh95@hotmail.com>
2020-03-25 16:20:29 +0200

    -.-

--- src/Board.js
+++ src/Board.js
@@ -1,112 +1,60 @@
 import React from "react";
+import Hole from "./Hole";
+
 function Board() {
   return (
     <div className="container">
       <div className="row mt-5">
         <div className="col">
           <div className="text-center">
-            <img
-              src="https://github.com/RhodesPeter/whack-a-mole-react/raw/master/public/assets/illustration-for-readme.png"
-              className="rounded"
-              alt="..."
-              height="150"
-              width="150"
-            />
+            <Hole />
           </div>
         </div>
 
         <div className="col">
           <div className="text-center">
-            <img
-              src="https://github.com/RhodesPeter/whack-a-mole-react/raw/master/public/assets/illustration-for-readme.png"
-              className="rounded"
-              alt="..."
-              height="150"
-              width="150"
-            />
+            <Hole />
           </div>
         </div>
         <div className="col">
           <div className="text-center">
-            <img
-              src="https://github.com/RhodesPeter/whack-a-mole-react/raw/master/public/assets/illustration-for-readme.png"
-              className="rounded"
-              alt="..."
-              height="150"
-              width="150"
-            />
+            <Hole />
           </div>
         </div>
       </div>
       <div className="row mt-5">
         <div className="col">
           <div className="text-center">
-            <img
-              src="https://github.com/RhodesPeter/whack-a-mole-react/raw/master/public/assets/illustration-for-readme.png"
-              className="rounded"
-              alt="..."
-              height="150"
-              width="150"
-            />
+            <Hole />
           </div>
         </div>
 
         <div className="col">
           <div className="text-center">
-            <img
-              src="https://github.com/RhodesPeter/whack-a-mole-react/raw/master/public/assets/illustration-for-readme.png"
-              className="rounded"
-              alt="..."
-              height="150"
-              width="150"
-            />
+            <Hole />
           </div>
         </div>
         <div className="col">
           <div className="text-center">
-            <img
-              src="https://github.com/RhodesPeter/whack-a-mole-react/raw/master/public/assets/illustration-for-readme.png"
-              className="rounded"
-              alt="..."
-              height="150"
-              width="150"
-            />
+            <Hole />
           </div>
         </div>
       </div>
       <div className="row mt-5">
         <div className="col">
           <div className="text-center">
-            <img
-              src="https://github.com/RhodesPeter/whack-a-mole-react/raw/master/public/assets/illustration-for-readme.png"
-              className="rounded"
-              alt="..."
-              height="150"
-              width="150"
-            />
+            <Hole />
           </div>
         </div>
 
         <div className="col">
           <div className="text-center">
-            <img
-              src="https://github.com/RhodesPeter/whack-a-mole-react/raw/master/public/assets/illustration-for-readme.png"
-              className="rounded"
-              alt="..."
-              height="150"
-              width="150"
-            />
+            <Hole />
           </div>
         </div>
         <div className="col">
           <div className="text-center">
-            <img
-              src="https://github.com/RhodesPeter/whack-a-mole-react/raw/master/public/assets/illustration-for-readme.png"
-              className="rounded"
-              alt="..."
-              height="150"
-              width="150"
-            />
+            <Hole />
           </div>
         </div>
       </div>
---
+++ src/Hole.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hole = () => {
+  return (
+    <div className="mound">
+      <div className="whack">
+        <div className="hole"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Hole;
