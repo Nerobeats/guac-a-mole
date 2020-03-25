3637d22e72fb7a3305034fd6b7f8d95e4e47d5e4
moe9195 <moerahmeh95@hotmail.com>
2020-03-25 18:14:01 +0200

    .

--- src/Hole.js
+++ src/Hole.js
@@ -2,14 +2,19 @@ import React from "react";
 
 const Hole = () => {
   return (
-    <>
-      {/* <div className="animated shake"> */}
-      <div className="animated slideInUp">
-        <div className="mole">
-          <div className="hole"></div>
-        </div>
-      </div>
-    </>
+    <div class="box">
+      <img
+        src="https://raw.githubusercontent.com/Nerobeats/guac-a-mole/master/src/assets/bebsi-nobg.png"
+        alt="mole"
+        class="mole-pic"
+      />
+      <img
+        src="https://raw.githubusercontent.com/Nerobeats/guac-a-mole/master/src/assets/hiclipart.com.png"
+        alt="mound"
+        class="mound-pic"
+      />
+      <div class="mask"></div>
+    </div>
   );
 };
 
