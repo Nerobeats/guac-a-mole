583fcf7a1faff5a73bd55e256486d9641d6f1602
NeroBeats <Hani.Hamshari@Hotmail.com>
2020-03-26 12:54:45 +0200

    fixed

--- src/Board.js
+++ src/Board.js
@@ -1,26 +1,37 @@
 import React from "react";
 import Hole from "./Hole";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 function mapping(size) {
-  let arr = [];
   let board = [];
-  for (let i = 0; i < size; i++) {
-    arr.push(
-      <div className="col">
-        <div className="text-center">
-          <Hole />
+  for (let j = 0; j < size; j++) {
+    let arr = [];
+    for (let i = 0; i < size; i++) {
+      arr.push(
+        <div className="col">
+          <div className="text-center">
+            <Hole />
+          </div>
         </div>
-      </div>
-    );
-  }
-
-  for (let i = 0; i < size; i++) {
+      );
+    }
     board.push(<div className="row mt-2">{arr}</div>);
   }
   return board;
 }
 function Board(props) {
-  return <div className="container">{mapping(props.size)}</div>;
+  return (
+    <div>
+      <div className="fixed-top text-right">
+        <br />
+        <Link to="/" className="btn btn-secondary btn-lg">
+          Home
+        </Link>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </div>
+      <div className="container"> {mapping(props.size)}</div>
+    </div>
+  );
 }
 const mapStateToProps = state => {
   return {
